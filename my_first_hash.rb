96e09f09bb492cb52cf0a16606b10bd4c3886009
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-25 21:50:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+your_hash = {"my_name" => "Rachel", "my_age" => 29, "my_city" => "Brooklyn"}
   
 end
 
